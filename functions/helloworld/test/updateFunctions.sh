41140d975c489f35b73360ccb0cd5ebb8c42d034
Campion Fellin <campionfellin@users.noreply.github.com>
2018-03-30 12:44:47 -0700

    Fix environmental variables

--- functions/helloworld/test/updateFunctions.sh
+++ functions/helloworld/test/updateFunctions.sh
@@ -5,18 +5,18 @@ ${FUNCTIONS_CMD} deploy helloGET --trigger-http
 echo '-----------------------------'
 ${FUNCTIONS_CMD} deploy helloHttp --trigger-http
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloBackground --trigger-topic $FUNCTIONS_TOPIC
+${FUNCTIONS_CMD} deploy helloBackground --trigger-topic $TOPIC
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloPubSub --trigger-topic $FUNCTIONS_TOPIC
+${FUNCTIONS_CMD} deploy helloPubSub --trigger-topic $TOPIC
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloGCS --trigger-bucket $FUNCTIONS_BUCKET
+${FUNCTIONS_CMD} deploy helloGCS --trigger-bucket $BUCKET
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloGCSGeneric --trigger-bucket $FUNCTIONS_BUCKET
+${FUNCTIONS_CMD} deploy helloGCSGeneric --trigger-bucket $BUCKET
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloError --trigger-topic $FUNCTIONS_TOPIC
+${FUNCTIONS_CMD} deploy helloError --trigger-topic $TOPIC
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloError2 --trigger-topic $FUNCTIONS_TOPIC
+${FUNCTIONS_CMD} deploy helloError2 --trigger-topic $TOPIC
 echo '-----------------------------'
-${FUNCTIONS_CMD} deploy helloError3 --trigger-topic $FUNCTIONS_TOPIC
+${FUNCTIONS_CMD} deploy helloError3 --trigger-topic $TOPIC
 echo '-----------------------------'
 ${FUNCTIONS_CMD} deploy helloTemplate --trigger-http
